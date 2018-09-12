dc8e05dd0e6903c943b5f32e0aa961b86b645d16
Keyti Toromanova <ktoromanova@luc.edu>
2018-09-11 21:54:31 -0500

    Added methods needed to make and display the first move on the board

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -15,7 +15,7 @@ public class TicTacToe {
 
         boardReset(board);
         // Display a message to the user, then display the board
-        System.out.println("\nHi, Welcome To The Tic Tac Toe Game!\n");
+        System.out.println("\n  Hi, Welcome To The Tic Tac Toe Game!\n");
         showBoard(board);
 
         // ask the user to enter their name
@@ -25,11 +25,32 @@ public class TicTacToe {
         System.out.print("  Please, enter Player 2's name: ");
         String player2Name = sc.next();
 
-        // Then ask the first player which symbol (x or o) he/she wants to play.
+        // Then ask the first player which symbol (x or o) he/she wants to play with.
         System.out.print("\n  " + player1Name+ ", choose a symbol to play with, \"x\" or \"o\"? ");
         char player1Symbol = sc.next().toLowerCase().charAt(0);
         char player2Symbol = (player1Symbol == 'x') ? 'o' : 'x';
-        System.out.print("  " + player2Name+ " will play with the '" + ((player1Symbol == 'x') ? 'o' : 'x' )+ "' symbol.");
+        System.out.print("  => " + player2Name+ " will play with the '" + ((player1Symbol == 'x') ? 'o' : 'x' )+ "' symbol.\n");
+
+        System.out.print("\n  " + player1Name + ", would you like to play first (y/n)? ");
+        char playerAns = sc.next().toLowerCase().charAt(0);
+
+        int turn; // 0 for player 1, 1 for player2
+        int remainCnt = SIZE * SIZE; // empty cell count
+
+        // this is going to be the first move
+        if (playerAns == 'y') {
+            turn = 0;
+            user1Play(board, player1Symbol); // player 1 places their first move
+        }
+        else {
+            turn = 1;
+            user2Play(board, player2Symbol); // player 2 placed their first move
+        }
+
+        // show the board
+        showBoard(board);
+        // decrement the number of remaining cells
+        remainCnt--;
 
     }
 
@@ -39,6 +60,7 @@ public class TicTacToe {
             for (int j = 0; j < bd[0].length; j++)
                 bd[i][j] = ' ';
     }
+
     // a function to display the board
     public static void showBoard(char[][] bd) {
         int numRow = bd.length;
@@ -74,5 +96,33 @@ public class TicTacToe {
             }
         }
         System.out.println();
+    }
+   public static void user1Play(char[][] bd, char u1turn)
+    {
+        System.out.print("\n  Please enter a row and a column to play: ");
+        int rowIndex = sc.nextInt();
+        int colIndex = sc.nextInt();
+
+        while (bd[rowIndex][colIndex] != ' ') {
+            System.out.print("\n   It looks like this cell is already taken....\nPlease enter a row and a column to play: ");
+            rowIndex = sc.nextInt();
+            colIndex = sc.nextInt();
+        }
+
+        bd[rowIndex][colIndex] = u1turn;
+    }
+    public static void user2Play(char[][] bd, char u2turn)
+    {
+        System.out.print("\n  Please enter a row and a column to play: ");
+        int rowIndex = sc.nextInt();
+        int colIndex = sc.nextInt();
+
+        while (bd[rowIndex][colIndex] != ' ') {
+            System.out.print("\n   It looks like this cell is already taken....\nPlease enter a row and a column to play: ");
+            rowIndex = sc.nextInt();
+            colIndex = sc.nextInt();
+        }
+
+        bd[rowIndex][colIndex] = u2turn;
     }
 }
